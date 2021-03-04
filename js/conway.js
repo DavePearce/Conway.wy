066e7826f7ac5a766803daccf44ea111046f0714
DavePearce <DavePearce@users.noreply.github.com>
2021-03-04 23:15:23 +0000

    deploy: 37e514ed7e84002bd54950137270bdbfef43a80d

--- js/conway.js
+++ js/conway.js
@@ -66,9 +66,10 @@ function main$loop$qQ5State$V(st) {
    }(st));
 }
 function model$State$type($) {
-   return $.cells.length === ($.width * $.height);
+   return ($.cells.length === ($.width * $.height)) && (($.width > 0) && ($.height > 0));
 }
 function model$init$Q4uintQ4uint$Q5State(width, height) {
+   let r;
     {
       const $6 = Math.floor(width / 20);
       width = $6;
@@ -77,9 +78,16 @@ function model$init$Q4uintQ4uint$Q5State(width, height) {
       const $7 = Math.floor(height / 20);
       height = $7;
    }
+   if(width <= 0)  {
+      width = 1;
+   }
+   if(height <= 0)  {
+      height = 1;
+   }
    return new Wy.Record({cells: Wy.array(false, width * height), width: width, height: height});
 }
 function model$click$IIQ5State$Q5State(x, y, s) {
+   let r;
    if((x >= 0) && ((y >= 0) && ((x < s.width) && (y < s.height))))  {
       let index = x + (y * s.width);
        {
@@ -90,43 +98,52 @@ function model$click$IIQ5State$Q5State(x, y, s) {
    return Wy.copy(s);
 }
 function model$update$Q5State$Q5State(state) {
+   let r;
    let ncells = Wy.copy(state.cells);
-   for(let x = 0;x < state.width;x = x + 1) {
-      for(let y = 0;y < state.height;y = y + 1) {
-         let c = model$count_living$Q4uintQ4uintQ5State$Q4uint(x, y, Wy.copy(state));
-         let i = x + (state.width * y);
-         if(model$alive$IIQ5State$Q4uint(x, y, Wy.copy(state)) === 1)  {
-            switch(c) {
-               case 0:
-               case 1: {
-                  ncells[i] = false;
-                  break;
-               }
-               case 2:
-               case 3: {
-                  break;
-               }
-               case 4:
-               case 5:
-               case 6:
-               case 7:
-               case 8: {
-                  ncells[i] = false;
-                  break;
-               }
-            }
-         } else  {
-            if(c === 3)  {
-               ncells[i] = true;
-            }
-         }
+   for(let y = 0;y < state.height;y = y + 1) {
+      for(let x = 0;x < state.width;x = x + 1) {
+         let i = x + (y * state.width);
+         ncells[i] = model$update_cell$IQ5State$B(i, Wy.copy(state));
       }
    }
    state.cells = Wy.copy(ncells);
    return Wy.copy(state);
 }
-function model$count_living$Q4uintQ4uintQ5State$Q4uint(x, y, state) {
+function model$update_cell$IQ5State$B(index, state) {
+   let out;
+   let count = model$count_living$Q4uintQ5State$Q4uint(index, Wy.copy(state));
+   if(state.cells[index])  {
+      switch(count) {
+         case 0:
+         case 1: {
+            return false;
+            break;
+         }
+         case 2:
+         case 3: {
+            return true;
+            break;
+         }
+         case 4:
+         case 5:
+         case 6:
+         case 7:
+         case 8: {
+            return false;
+            break;
+         }
+      }
+   } else  {
+      if(count === 3)  {
+         return true;
+      }
+   }
+   return false;
+}
+function model$count_living$Q4uintQ5State$Q4uint(index, state) {
    let r;
+   let x = index % state.width;
+   let y = Math.floor(index / state.width);
    let count = model$alive$IIQ5State$Q4uint(x - 1, y - 1, Wy.copy(state));
     {
       const $9 = count + model$alive$IIQ5State$Q4uint(x - 1, y, Wy.copy(state));
@@ -160,18 +177,10 @@ function model$count_living$Q4uintQ4uintQ5State$Q4uint(x, y, state) {
 }
 function model$alive$IIQ5State$Q4uint(x, y, state) {
    let r;
-   if((x < 0) || (x >= state.width))  {
+   if((x < 0) || ((x >= state.width) || ((y < 0) || ((y >= state.height) || (!state.cells[x + (y * state.width)])))))  {
       return 0;
    } else  {
-      if((y < 0) || (y >= state.height))  {
-         return 0;
-      } else  {
-         if(state.cells[x + (y * state.width)])  {
-            return 1;
-         } else  {
-            return 0;
-         }
-      }
+      return 1;
    }
 }
 function view$draw(canvas, state) {
